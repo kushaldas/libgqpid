d6627a971906ea477d76bed210d918e0fb5f5e13
Kushal Das <kdas@redhat.com>
2011-09-08 22:48:26 +0530

    API documentation added & format fixed.

--- gqpidsession.cpp
+++ gqpidsession.cpp
@@ -16,12 +16,12 @@ struct _GQpidSession
 
     _GQpidSession(Connection &c)
     {
-        session = c.createSession();
+        session = c.createSession ();
     }
 };
 
 GQpidSession*
-g_qpid_session_new(Connection &c)
+g_qpid_session_new (Connection &c)
 {
     GQpidSession *session;
     session = new GQpidSession (c);
@@ -29,6 +29,18 @@ g_qpid_session_new(Connection &c)
     return session;
 }
 
+/**
+ * g_qpid_session_create_sender:
+ * @session: a #GQpidSession* object
+ * @add: gchar* used as address of sender
+ *
+ * Creates a new sender object.
+ *
+ * Return value: a newly allocated #GQpidSender.
+ *   You must close it with g_qpid_sender_close()
+ *   and then free it with g_qpid_sender_free() when
+ *   are finished with it.
+ **/
 GQpidSender*
 g_qpid_session_create_sender(GQpidSession *session, gchar *add)
 {
@@ -38,6 +50,18 @@ g_qpid_session_create_sender(GQpidSession *session, gchar *add)
     return sender;
 }
 
+/**
+ * g_qpid_session_create_receiver:
+ * @session: a #GQpidSession* object
+ * @add: gchar* used as address of receiver
+ *
+ * Creates a new receiver object.
+ *
+ * Return value: a newly allocated #GQpidReceiver.
+ *   You must close it with g_qpid_receiver_close()
+ *   and then free it with g_qpid_receiver_free() when
+ *   are finished with it.
+ **/
 GQpidReceiver*
 g_qpid_session_create_receiver(GQpidSession *session, gchar *add)
 {
@@ -47,25 +71,49 @@ g_qpid_session_create_receiver(GQpidSession *session, gchar *add)
     return rcv;
 }
 
+/**
+ * g_qpid_session_create_receiver:
+ * @session: a #GQpidSession* object
+ *
+ * Returns the total number of messages received and waiting to be
+ * fetched by all Receivers belonging to this session.
+ *
+ * Return value: This is the total number of available messages
+ * across all receivers on this session.
+ **/
 gint
 g_qpid_session_get_receivable (GQpidSession *s)
 {
     g_return_val_if_fail (s != NULL, -1);
 
-    return s->session.getReceivable();
+    return s->session.getReceivable ();
 }
 
+/**
+ * g_qpid_session_acknowledge:
+ * @session: a #GQpidSession* object
+ *
+ * Acknowledges all outstanding messages that have
+ * been received by the application on this session.
+ **/
 void
-g_qpid_session_acknowledge(GQpidSession *session)
+g_qpid_session_acknowledge (GQpidSession *session)
 {
     g_return_if_fail (session != NULL);
 
-    session->session.acknowledge();
+    session->session.acknowledge ();
     return;
 }
 
+/**
+ * g_qpid_session_release:
+ * @session: a #GQpidSession* object
+ * @msg: a #GQpidMessage* message
+ *
+ * Releases the specified message.
+ **/
 void
-g_qpid_session_release(GQpidSession *session, GQpidMessage *msg)
+g_qpid_session_release (GQpidSession *session, GQpidMessage *msg)
 {
     g_return_if_fail (session != NULL);
 
@@ -73,8 +121,15 @@ g_qpid_session_release(GQpidSession *session, GQpidMessage *msg)
     return;
 }
 
+/**
+ * g_qpid_session_reject:
+ * @session: a #GQpidSession* object
+ * @msg: a #GQpidMessage* message
+ *
+ * Rejects the specified message.
+ **/
 void
-g_qpid_session_reject(GQpidSession *session, GQpidMessage *msg)
+g_qpid_session_reject (GQpidSession *session, GQpidMessage *msg)
 {
     g_return_if_fail (session != NULL);
 
@@ -82,8 +137,14 @@ g_qpid_session_reject(GQpidSession *session, GQpidMessage *msg)
     return;
 }
 
+/**
+ * g_qpid_session_close:
+ * @session: a #GQpidSession* object
+ *
+ * Closes the session.
+ **/
 void
-g_qpid_session_close(GQpidSession *session)
+g_qpid_session_close (GQpidSession *session)
 {
 
     g_return_if_fail (session != NULL);
@@ -91,9 +152,15 @@ g_qpid_session_close(GQpidSession *session)
     session->session.close ();
 }
 
-
+/**
+ * g_qpid_session_free:
+ * @session: a #GQpidSession* object
+ *
+ * Frees the session, must be called after you close the
+ * session.
+ **/
 void
-g_qpid_session_free(GQpidSession *session)
+g_qpid_session_free (GQpidSession *session)
 {
     delete session;
 }
