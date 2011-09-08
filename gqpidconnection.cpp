2659b0a6d223ac215689664a93a520844145f411
Kushal Das <kdas@redhat.com>
2011-09-08 15:56:38 +0530

    Documentation & format fixing.

--- gqpidconnection.cpp
+++ gqpidconnection.cpp
@@ -17,9 +17,20 @@ struct _GQpidConnection
     _GQpidConnection(const gchar *broker): conn(broker, "") {}
 };
 
+/**
+ * g_qpid_connection_new:
+ * @broker: the broker name
+ *
+ * Creates a new connection to the given qpid broker.
+ *
+ * Return value: a newly allocated #GQpidConnection.
+ *   You must close it with g_qpid_connection_close()
+ *   and then free it with g_qpid_connection_free() when
+ *   are finished with it.
+ **/
 
 GQpidConnection*
-g_qpid_connection_new(gchar* broker)
+g_qpid_connection_new (gchar* broker)
 {
     GQpidConnection *conn;
     conn = new GQpidConnection (broker);
@@ -27,8 +38,14 @@ g_qpid_connection_new(gchar* broker)
     return conn;
 }
 
+/**
+ * g_qpid_connection_open:
+ * @conn: an instance of #GQpidConnection*
+ *
+ * Opens the given connection.
+ **/
 void
-g_qpid_connection_open(GQpidConnection *conn)
+g_qpid_connection_open (GQpidConnection *conn)
 {
 
     g_return_if_fail (conn != NULL);
@@ -37,8 +54,19 @@ g_qpid_connection_open(GQpidConnection *conn)
 
 }
 
+/**
+ * g_qpid_connection_create_session:
+ * @conn: an instance of #GQpidConnection*
+ *
+ * Creates a new #GQPidSession object from the connection
+ * and returns it.
+ *
+ * Return value: a newly allocated #GQpidSession object. You
+ *   must free it with g_qpid_session_free after you are
+ *   finished wit it.
+ **/
 GQpidSession*
-g_qpid_connection_create_session(GQpidConnection *conn)
+g_qpid_connection_create_session (GQpidConnection *conn)
 {
     g_return_val_if_fail (conn != NULL, NULL);
 
@@ -48,19 +76,34 @@ g_qpid_connection_create_session(GQpidConnection *conn)
     return session;
 }
 
+/**
+ * g_qpid_connection_is_open:
+ * @conn: an instance of #GQpidConnection*
+ *
+ * Cheacks if the given is open or not.
+ *
+ * Return value: TRUE if the connection is open or
+ *   returns FALSE.
+ **/
 gboolean
-g_qpid_connection_is_open(GQpidConnection *conn)
+g_qpid_connection_is_open (GQpidConnection *conn)
 {
 
     g_return_val_if_fail (conn != NULL, FALSE);
 
-    gboolean b = conn->conn.isOpen();
+    gboolean b = conn->conn.isOpen ();
     return b;
 
 }
 
+/**
+ * g_qpid_connection_close:
+ * @conn: an instance of #GQpidConnection*
+ *
+ * Closes the connection.
+ **/
 void
-g_qpid_connection_close(GQpidConnection *conn)
+g_qpid_connection_close (GQpidConnection *conn)
 {
 
     g_return_if_fail (conn != NULL);
@@ -69,8 +112,15 @@ g_qpid_connection_close(GQpidConnection *conn)
 }
 
 
+/**
+ * g_qpid_connection_free:
+ * @conn: an instance of #GQpidConnection*
+ *
+ * Deallocates all memory and frees the object. Must be
+ * called after you close the connection object.
+ **/
 void
-g_qpid_connection_free(GQpidConnection *conn)
+g_qpid_connection_free (GQpidConnection *conn)
 {
     delete conn;
 }
